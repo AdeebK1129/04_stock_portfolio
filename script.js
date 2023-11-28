5ddb19298d7b4ef41feaf0c31cf6b376917b82d7
Adeeb <adeeb19909@gmail.com>
2023-11-27 22:33:27 -0500

    Adding bootstrap elemens and fixing fetch from Stock List API, setting up second fetch to time set API

--- script.js
+++ script.js
@@ -1,26 +1,35 @@
 // NASDAQ fetch
 
-const axios = require('axios');
-
+const url = 'https://twelve-data1.p.rapidapi.com/stocks?exchange=NASDAQ&format=json';
 const options = {
-  method: 'GET',
-  url: 'https://twelve-data1.p.rapidapi.com/stocks',
-  params: {
-    exchange: 'NASDAQ',
-    format: 'json'
-  },
-  headers: {
-    'X-RapidAPI-Key': '5a7260a38dmsh17961d8946f04d4p1eec7bjsne59a5a7977e5',
-    'X-RapidAPI-Host': 'twelve-data1.p.rapidapi.com'
-  }
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '5a7260a38dmsh17961d8946f04d4p1eec7bjsne59a5a7977e5',
+		'X-RapidAPI-Host': 'twelve-data1.p.rapidapi.com'
+	}
 };
 
-try {
-	const response = await axios.request(options);
-	console.log(response.data);
-} catch (error) {
-	console.error(error);
+const initialize = async function(){
+  try {
+      const response = await fetch(url, options);
+      const result = await response.json();
+      console.log(result.data[0]);
+  } catch (error) {
+      console.error(error);
+  }
 }
 
+initialize()
+
+let SYMBOL = 'TSLA';
+let APIKEY = 'test';
+
+const url2 = `https://api.twelvedata.com/time_series?symbol=${SYMBOL}&interval=1min&apikey=${APIKEY}`
+fetch(url2)
+.then(response => response.json())
+    .then(data => {
+        console.log(data[0])
+      })
+    .catch(err => console.log(err));
 
 
